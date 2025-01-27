bac90aba9298d034c9c6f4811598b01f57a6216e
Wenxuan Shen <bushigemen114@gmail.com>
2025-01-27 15:41:18 +0800

    fix: 只在 macOS 下应用这些更改

--- menu.py
+++ menu.py
@@ -1885,11 +1885,10 @@ class SettingsMenu(FluentWindow):
         self.resize(width, height)
 
         # macOS 标题栏显示 Windows 一样的按钮
-        self.titleBar.minBtn.setVisible(True)
-        self.titleBar.maxBtn.setVisible(True)
-        self.titleBar.closeBtn.setVisible(True)
-
-        self.setWindowFlags(Qt.WindowType.Window) # macOS 自动给焦点
+        if sys.platform == 'darwin':
+            self.titleBar.minBtn.setVisible(True)
+            self.titleBar.maxBtn.setVisible(True)
+            self.titleBar.closeBtn.setVisible(True)
 
         self.setWindowTitle('Class Widgets - 设置')
         self.setWindowIcon(QIcon(f'{base_directory}/img/logo/favicon-settings.ico'))
